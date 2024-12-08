f639154cc4857779707b97e7860b479da49816a4
tribeti <nguyenductri28072004@gmail.com>
2024-12-08 11:48:49 +0700

    zkjvlkxc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
-import PostDetail from './components/PostDetail';
-import CreatePost from './components/CreatePost';
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import PostDetail from "./components/PostDetail";
+import CreatePost from "./components/CreatePost";
 
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/post/:id" element={<PostDetail />} />
-          <Route path="/create-post" element={<CreatePost />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/post/:id" element={<PostDetail />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+      <ToastContainer />
+    </Router>
   );
 }
 
